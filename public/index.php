5aef888ea81e3744dcb1dc98e35717e9069e269a
Cyril Vermande <cyril@cyrilwebdesign.com>
2020-01-04 14:54:39 +0100

    Add GET action

--- public/index.php
+++ public/index.php
@@ -6,9 +6,30 @@ use Slim\Factory\AppFactory;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
+$config = [
+    'resources' => [
+        'book' => [
+            'actions' => [
+                'get' => [
+                    'method' => 'GET',
+                    'uri' => '/books/{id}',
+                    'handler' => 'SlimPlatform\Action\Get'
+                ]
+            ]
+        ]
+    ]
+];
+
 $app = AppFactory::create();
 $app->get('/', function (Request $request, Response $response) {
     return $response;
 });
+
+foreach ($config['resources'] as $name => $resource) {
+    foreach ($resource['actions'] as $action) {
+        $app->map([$action['method']], $action['uri'], $action['handler']);
+    }
+}
+
 $app->addErrorMiddleware(true, true, true);
 $app->run();
---
+++ src/Action/Get.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SlimPlatform\Action;
+
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+class Get
+{
+    public function __invoke(Request $request, Response $response, $args): Response
+    {
+        $response->getBody()->write(json_encode(['message' => 'Ok']));
+
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+}
