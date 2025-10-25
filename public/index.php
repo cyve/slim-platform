352bb1b821e8032193f01984ea44c66de8c8f2f3
cyve <madislak@yahoo.fr>
2025-10-25 12:03:39 +0200

    Add middleware to rewrite path

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use Middlewares\BasePath;
+
 require dirname(__DIR__).'/vendor/autoload.php';
 
 if (is_readable(dirname(__DIR__).'/.env')) {
@@ -7,5 +9,8 @@ if (is_readable(dirname(__DIR__).'/.env')) {
 }
 
 $app = new SlimPlatform\App();
+if ($basePath = $_SERVER['REDIRECT_BASE'] ?? $_SERVER['BASE'] ?? null) {
+    $app->addMiddleware(new BasePath($basePath));
+}
 $app->addMiddleware(new Middlewares\ResponseTime());
 $app->run();
